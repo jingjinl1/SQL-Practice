d34a7dbd5ce7d3c41e718707eb64e5ad89162705
jingjinl1 <jingjinli266@gmail.com>
2024-11-16 09:53:34 +1000

    update practice day 4

--- Practice Day 4.sql
+++ Practice Day 4.sql
@@ -1,13 +1,36 @@
 /*
 Created by: JJ
 created on: 15/11/2024
-Description: GRROUP BY | W the average invoice totals by city
+Description: GROUP BY | What are the average invoice totals by city for 
+only the city starts with L?
 */
 SELECT
   i.BillingCity,
   ROUND(AVG(i.total),2) AS AverageTotal
 FROM Invoice AS i
+WHERE
+  i.BillingCity LIKE 'L%'
 GROUP BY
   i.BillingCity
 ORDER BY
+  i.BillingCity;
+  
+  
+/*
+What are the average invoice totals by each city 
+and filter the average to over $5.00, 
+and only display the city that starts with B
+*/
+SELECT
+i.BillingCity,
+ROUND(AVG(i.total),2) AS 'Average total greater than 5'
+FROM
+  Invoice as i
+WHERE
+  i.BillingCity LIKE 'B%'
+GROUP BY
   i.BillingCity
+HAVING
+  AVG(i.total) > 5
+ORDER BY
+  i.BillingCity;
