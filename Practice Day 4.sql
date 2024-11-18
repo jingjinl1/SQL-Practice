5c926d825f8d928e5867b2149e8541cdd8155cee
jingjinl1 <jingjinli266@gmail.com>
2024-11-18 14:16:18 +1000

    update parctice day 4

--- Practice Day 4.sql
+++ Practice Day 4.sql
@@ -22,7 +22,7 @@ and filter the average to over $5.00,
 and only display the city that starts with B
 */
 SELECT
-i.BillingCity,
+i.BillingCity AS 'BillingCity Starts with B',
 ROUND(AVG(i.total),2) AS 'Average total greater than 5'
 FROM
   Invoice as i
@@ -33,4 +33,37 @@ GROUP BY
 HAVING
   AVG(i.total) > 5
 ORDER BY
+  i.BillingCity;
+  
+  
+/*
+Description: Multiple Grouping | What are the average invoice totals by Country and by city
+*/
+SELECT
+  i.BillingCountry,
+  i.BillingCity,
+  ROUND(AVG(i.total),2) AS 'Average Total'
+FROM 
+  Invoice as i
+GROUP BY
+  i.BillingCountry,
+  i.BillingCity
+ORDER BY
+  i.BillingCountry;
+
+  
+/*
+Code challenge: Calculate Average Spend per City
+Results: Should have 2 colmns, City and AverageSpending;
+         Average spending should be rounded to 2 decimals;
+         Order by city name ASC
+*/   
+SELECT 
+  i.BillingCity AS City,
+  ROUND(AVG(i.total),2) AS AverageSpending
+FROM 
+  Invoice AS i
+GROUP BY
+  i.BillingCity
+ORDER BY 
   i.BillingCity;
